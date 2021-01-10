f1f6f1a70b5d15e3aa4d47ee1a70070f7a9ddaac
Woozy1 <Woozy1@users.noreply.github.com>
2021-01-10 15:04:36 +0100

    Updated MainActivity.java

--- app/src/main/java/com/example/cloudreminder/MainActivity.java
+++ app/src/main/java/com/example/cloudreminder/MainActivity.java
@@ -4,11 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView groups;
+    private String url = "https://cloudreminder-dev.azurewebsites.net/api/v1/Group";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        groups = (TextView) findViewById(R.id.groups);
     }
+
+    public  void getGroups(View view){
+        JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener)
+        {
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("ApiKey", "SecretKey");
+                return params;
+            }
+        };
+        requestQueue.add(request);
+
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response){
+            ArrayList<String> data = new ArrayList<>();
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject object =response.getJSONObject(i);
+                    String name = object.getString("name");
+                    data.add(name);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+                }
+            }
+            groups.setText("");
+            for (String row: data){
+                String currentText = groups.getText().toString();
+                groups.setText(currentText + "\n\n" + row);
+            }
+        }
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+
 }
